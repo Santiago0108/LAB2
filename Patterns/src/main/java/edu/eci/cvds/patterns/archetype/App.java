f59c3b48363f18644b83fbd9144c96fb3c591f90
Erick Montero <e.santiagom.s@gmail.com>
2024-02-03 09:54:02 -0500

    LAB2

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -0,0 +1,22 @@
+package edu.eci.cvds.patterns.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name="";
+        for( String param : args) {
+            name += param+" ";
+        }
+        if("".contentEquals(name)) {
+            System.out.println("Hello world!");
+        }
+        else {
+            System.out.println("Hello "+name+"!");
+        }
+    }
+}
